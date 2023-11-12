5dfc695edd66a451195fdf688e800930da33fe69
Catrin <catrin.lam@gmail.com>
2023-11-12 17:30:55 +0000

    Set up Rest Framework

--- yourthoughts/api/views.py
+++ yourthoughts/api/views.py
@@ -3,24 +3,31 @@ from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from . import models
+from . import serializers
 
 # Create your views here.
 
+@api_view(['GET'])
 def index(request):
     #survey_list = models.Survey.objects.order_by('module')
     #output = ', '.join([q.module for q in survey_list])
     #return HttpResponse(output)
     module_list = models.Survey.objects.all()
-    output = '\n'.join([q.module for q in module_list])
-    return HttpResponse(output)
+    #output = '\n'.join([q.module for q in module_list])
+    serializer = serializers.SurveySerializer(module_list, many=True)
+    return Response(serializer.data)
 
+@api_view(['GET'])
 def survey(request, module_wanted):
-    module_id = models.Survey.objects.get(module=module_wanted)
-    return HttpResponse("Module Survey %s." % module_id)
+    module_name = models.Survey.objects.get(module=module_wanted)
+    serializer = serializers.SurveySerializer(module_name, many=False)
+    return Response(serializer.data)
 
+@api_view(['GET'])
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    responses = models.Submission.objects.all()
+    serializer = serializers.SubmissionSerializer(responses, many=True)
+    return Response(serializer.data)
     # return render('hi',request)
 
 # def survey(request, survey_id):
