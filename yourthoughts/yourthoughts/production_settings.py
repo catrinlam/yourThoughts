c983275377b46455126d15e0b07a394c805eaf2c
Catrin <catrin.lam@gmail.com>
2023-11-26 21:07:19 +0000

    docker working!

--- yourthoughts/yourthoughts/production_settings.py
+++ yourthoughts/yourthoughts/production_settings.py
@@ -28,9 +28,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "https://yourthoughts.me"
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://yourthoughts.me'
 ]
 
 REST_FRAMEWORK = {
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -72,8 +73,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'yourthoughts.urls'
