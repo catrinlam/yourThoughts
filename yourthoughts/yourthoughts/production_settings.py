ac4013f424b08c7ce8692988990bbba431557a4c
Catrin <95254982+winter7eaf@users.noreply.github.com>
2024-01-25 13:51:16 +0000

    Update production_settings.py
    added allowed host

--- yourthoughts/yourthoughts/production_settings.py
+++ yourthoughts/yourthoughts/production_settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-12o%y#)+9sr8qic7=3e@-%tk6o#$ev_9(cnfrm+!g=p@*(^j5%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
