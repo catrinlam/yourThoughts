59f97753a194cecd175b4ebf32cf462e676570c2
Catrin <catrin.lam@gmail.com>
2024-02-14 16:20:18 +0000

    add back sqlite and no more collect static in backend docker

--- yourthoughts/entrypoint.sh
+++ yourthoughts/entrypoint.sh
@@ -9,7 +9,7 @@ python manage.py makemigrations --settings=yourthoughts.production_settings
 python manage.py migrate --settings=yourthoughts.production_settings
 
 # Collect static files
-python manage.py collectstatic --noinput --settings=yourthoughts.production_settings
+#python manage.py collectstatic --noinput --settings=yourthoughts.production_settings
 
 # Start the Django server
 exec python manage.py runserver 0.0.0.0:8000 --settings=yourthoughts.production_settings
