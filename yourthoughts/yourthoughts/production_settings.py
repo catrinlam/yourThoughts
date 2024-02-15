36913109028885bee8b81067f89cc89031499cb1
Catrin <catrin.lam@gmail.com>
2024-02-15 12:14:44 +0000

    added origins in settings for localhost:10000

--- yourthoughts/yourthoughts/production_settings.py
+++ yourthoughts/yourthoughts/production_settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://yourthoughts.me'
+    'https://yourthoughts.me',
+    'http://localhost:10000'
 ]
 
 CSRF_TRUSTED_ORIGINS = ["https://yourthoughts.me"]
